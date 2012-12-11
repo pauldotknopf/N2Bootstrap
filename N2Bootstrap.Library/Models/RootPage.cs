b44ede94d6216c29c76d5486912e101a6fe5a40c
Paul Knopf <theonlylawislove@gmail.com>
2012-12-10 20:38:58 -0800

    Added a root page with email settings. Added mail sender service that uses configured email settings.

---
+++ N2Bootstrap.Library/Models/RootPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using N2;
+using N2.Definitions;
+using N2.Details;
+using N2.Integrity;
+using N2.Web.UI;
+
+namespace N2Bootstrap.Library.Models
+{
+    [PageDefinition("Root Page (fallback)",
+        Description = "A fallback root page used to organize start pages. This root can be replaced or inherited in a web application project.",
+        SortOrder = 0,
+        InstallerVisibility = N2.Installation.InstallerHint.PreferredRootPage,
+        IconUrl = "{ManagementUrl}/Resources/icons/page_gear.png",
+        TemplateUrl = "{ManagementUrl}/Myself/Root.aspx")]
+    [RestrictParents(AllowedTypes.None)]
+    [TabContainer("Search", "Search", 200)]
+    [TabContainer("Email", "Email", 100)]
+    [FieldSetContainer("EmailField", "Email", 100, ContainerName = "Email")]
+    [WithManageableSearch(ContainerName = "Search")]
+    public class RootPage : ContentItem, IRootPage, ISystemNode
+    {
+        [EditableText("Host", 1, ContainerName = "EmailField")]
+        public virtual string Host { get; set; }
+
+        [EditableNumber("Port", 2, ContainerName = "EmailField")]
+        public virtual int Port { get; set; }
+
+        [EditableText("User", 3, ContainerName = "EmailField")]
+        public virtual string User { get; set; }
+
+        [EditableText("Password", 4, ContainerName = "EmailField")]
+        public virtual string Password { get; set; }
+
+        [EditableCheckBox("Use SSL", 5, ContainerName = "EmailField")]
+        public virtual bool UseSSL { get; set; }
+
+        [EditableCheckBox("Use default credentials", 6, ContainerName = "EmailField")]
+        public virtual bool UseDefaultCredentials { get; set; }
+    }
+}
---
+++ N2Bootstrap.Library/Services/MailSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using N2.Engine;
+using N2.Web.Mail;
+
+namespace N2Bootstrap.Library.Services
+{
+    [Service(typeof(IMailSender), Replaces=typeof(SmtpMailSender))]
+    public class MailSender : SmtpMailSender
+    {
+        protected override SmtpClient GetSmtpClient()
+        {
+            Models.RootPage root = null;
+            try
+            {
+                root = N2.Find.RootItem as Models.RootPage;
+                if (root == null)
+                    throw new InvalidOperationException("The root page must be present and be a bootstrap root.");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Mail sender can't find root item. " + ex.Message);
+                return new SmtpClient("localhost", 25);
+            }
+
+            var client = new SmtpClient(root.Host, root.Port);
+            client.UseDefaultCredentials = root.UseDefaultCredentials;
+            client.EnableSsl = root.UseSSL;
+            client.Credentials = root.UseDefaultCredentials ? CredentialCache.DefaultNetworkCredentials : new NetworkCredential(root.User, root.Password);
+            return client;
+        }
+    }
+}
