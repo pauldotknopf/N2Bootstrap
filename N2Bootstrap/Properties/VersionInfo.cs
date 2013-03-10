e9c7f769544c48ed741bbe83b555cac6e861c458
Paul Knopf <theonlylawislove@gmail.com>
2013-03-10 19:48:36 -0400

    Updating to latest n2cms

--- N2Bootstrap/Properties/VersionInfo.cs
+++ N2Bootstrap/Properties/VersionInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.12")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
-[assembly: AssemblyInformationalVersion("1.0.0.12")]
+[assembly: AssemblyVersion("1.0.0.15")]
+[assembly: AssemblyFileVersion("1.0.0.15")]
+[assembly: AssemblyInformationalVersion("1.0.0.15")]
 
 
